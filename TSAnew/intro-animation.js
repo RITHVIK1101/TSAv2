64a64f84d2b6e6a4d8c06c12b397c57ad3e20d64
Rithvik <65542427+RITHVIK1101@users.noreply.github.com>
2024-03-01 14:13:59 -0800

    Delete TSAnew directory

--- TSAnew/intro-animation.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var textOverlay = document.querySelector('.text-overlay');
-    var opacity = 1 - (scrollPosition / (window.innerHeight / 2));
-    textOverlay.style.opacity = opacity;
-    var videoBackground = document.getElementById('video-background');
-    var blurAmount = (scrollPosition / (window.innerHeight / 2)) * 10;
-    videoBackground.style.filter = 'blur(' + blurAmount + 'px)';
-});
